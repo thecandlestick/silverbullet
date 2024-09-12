4b481d6b6a334bedfe9fe3d7e9d9cefb8dc0a00b
Jack Manhardt <jackmanhardt@gmail.com>
2024-09-12 22:18:30 +0000

    revert katex

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -29,7 +29,7 @@ cp -r dist_plug_bundle/_plug/* website_build/_plug/
 #echo "And additional ones"
 curl https://raw.githubusercontent.com/silverbulletmd/silverbullet-mermaid/main/mermaid.plug.js > website_build/_plug/mermaid.plug.js
 #curl https://raw.githubusercontent.com/silverbulletmd/silverbullet-graphview/main/graphview.plug.js > website_build/_fs/_plug/graphview.plug.js
-curl https://raw.githubusercontent.com/silverbulletmd/silverbullet-katex/main/katex.plug.js > website_build/_plug/katex.plug.js
+#curl https://raw.githubusercontent.com/silverbulletmd/silverbullet-katex/main/katex.plug.js > website_build/_plug/katex.plug.js
 
 echo "But remove some plugs"
 rm -rf website_build/_plug/{plugmd}.plug.js
